8b63c396b16b693c5461e80f9849191b739ae918
tarungajamoni <saitarun.goud1998@gmail.com>
2025-06-13 20:15:09 +0530

    JS: bind polyfill modify

--- javascript/polyfills/bind.js
+++ javascript/polyfills/bind.js
@@ -20,12 +20,19 @@ const funcexe1 = func.myBind1(obj, "simple");
 funcexe1();
 
 //handling edge cases
-Function.prototype.myBind2 = function (scope, ...args) {
+Function.prototype.myBind2 = function (scope, ...bindArgs) {
   scope = scope ?? globalThis;
-  scope.temp = this;
-  return function () {
-    return scope.temp(...args);
+  const fn = this;
+  return function (...callArgs) {
+    //callArgs are to support args passed during invocation
+    scope.temp = fn;
+    const result = scope.temp(...[...bindArgs, ...callArgs]);
+    delete scope.temp;
+    return result;
   };
 };
 const funcexe2 = func.myBind2(obj, "test");
 funcexe2();
+//or below
+// const funcexe2 = func.myBind2(obj);
+// funcexe2('test1');
