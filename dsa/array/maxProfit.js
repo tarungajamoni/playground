956d3bab802492f94b7d8449ea6952f0c44f64e4
tarungajamoni <saitarun.goud1998@gmail.com>
2025-06-24 11:38:36 +0530

    DS: Array/maxProfit update

--- dsa/array/maxProfit.js
+++ dsa/array/maxProfit.js
@@ -1,9 +1,11 @@
 const maxProfit = (arr) => {
   let profit = 0;
-  for (let i = 0; i < arr.length - 1; i++) {
-    profit = Math.max(profit, arr[i + 1] - arr[i]);
+  let buyPrice = arr[0];
+  for (let i = 1; i < arr.length - 1; i++) {
+    if (buyPrice > arr[i]) buyPrice = arr[i];
+    profit = Math.max(profit, arr[i] - buyPrice);
   }
   return profit;
 };
 
-console.log(maxProfit([1, 7, 5, 3, 6, 4]));
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
