9c1153bc176c83f40f6c07b392609b73adcb9211
tarungajamoni <saitarun.goud1998@gmail.com>
2025-06-27 17:49:05 +0530

    DS: Array/subArrayChunk update

--- dsa/array/subArray.js
+++ dsa/array/subArray.js
@@ -1,11 +1,11 @@
-const subArray = (arr) => {
+const subArray = (arr, size) => {
   const new_arr = [];
   for (i = 0; i < arr.length; i++) {
-    const subArrayIndex = Math.floor(i / 3);
+    const subArrayIndex = Math.floor(i / size);
     if (!new_arr[subArrayIndex]) new_arr[subArrayIndex] = [];
     new_arr[subArrayIndex].push(arr[i]);
   }
   return new_arr;
 };
 
-console.log(subArray([1, 2, 3, 4, 5, 6, 7, 8])); //[ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8 ] ]
+console.log(subArray([1, 2, 3, 4, 5, 6, 7, 8], 3)); //[ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8 ] ]
